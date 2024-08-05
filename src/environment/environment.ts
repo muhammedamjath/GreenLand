64464a9f434e3e074225032916e5f99cebbd7165
muhammedamjath <muhammedamjath0@gmail.com>
2024-08-05 13:29:06 +0530

    ip added

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    baseUrl:'http://localhost:3000',
-    socketUrl:'http://localhost:3000'
+    baseUrl:'http://54.253.101.173',
+    socketUrl:'http://54.253.101.173'
 }
